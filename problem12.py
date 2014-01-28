b95704e1a1c193d1ff2fce1bedcc335a1516567a
Vikas Reddy <vikas@occipital.com>
2014-01-28 01:29:06 -0700

    solved problem 12

---
+++ problem12.py
@@ -0,0 +1,22 @@
+triangle_num = 1
+num_on = 2
+
+while True:
+    num_divisors = 0
+    cur = 1
+    print "checking %d" % (triangle_num,)
+    end = triangle_num
+    while True:
+        if triangle_num % cur == 0:
+            num_divisors += 2
+            end = (triangle_num / cur) - 1
+        cur +=1
+        if cur > end:
+            break
+
+    print "triangle_num: %d, divisors: %d" % (triangle_num, num_divisors)
+    if num_divisors > 500:
+        break
+    
+    triangle_num += num_on
+    num_on += 1
